88e5d876bc4ea31b90b2fd15616fd902e1a59c1b
gjotuur <v.chekhovskyi@proton.me>
2025-12-01 00:16:54 +0200

    Initial - added struct for group, added init func for it

---
+++ discrete_log.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+//We work with multiplicative groups here - so we create a structure to save them (generator, module - prime number, order = p-1)
+//Since the size of such a struct is comparably small, we can use just stack, without direct memory allocation
+typedef struct 
+{
+    uint64_t module;
+    uint64_t generator;
+    uint64_t order;
+
+}AlgebraicGroup;
+
+
+void init_group(AlgebraicGroup* group, uint64_t module, uint64_t generator, uint64_t order);
+
+int main(){
+
+    return 0;
+}
+
+//Simple _init through pointer
+void init_group(AlgebraicGroup* group, uint64_t module, uint64_t generator, uint64_t order){
+    group->generator = generator;
+    group->module = module;
+    group->order = module-1;
+}
